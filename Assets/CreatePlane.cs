ed9ec0f44221f61f70e5d13e89a6b0fd970de88f
megbeckim <timothy@Timothys-MacBook-Air.local>
2015-09-03 21:14:37 -0400

    this time with scenes

---
+++ Assets/CreatePlane.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (var i = 0; i < Input.touchCount; ++i) {
+			if (Input.GetTouch(i).phase == TouchPhase.Began) {
+				               
+			}
+		}
+	}
+}
