c2514f7c70928a6102a2c75942d63cf9b0079471
dariol <dario.nycjava@gmail.com>
2015-09-04 14:03:40 -0400

    fixed AR - added second camera for running w/o Tango in editor
    also added ball, table (temporarily fixed position for development)
    TODO:
    tango: fix point cloud mapping to AR, test find/get plane to position
    table
    game: make ball bounce, shoot ball, add cups to table etc etc

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	float speed = 4.0f; //TODO: adjust
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+// how's it's usually done:
+		/*
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float zAxisValue = Input.GetAxis("Vertical");
+		if(Camera.current != null)
+		{
+			Camera.current.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+		}
+		*/
+
+// use left/roght arrows to move camera left and right, up/down to move in/out z axis
+
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			Camera.current.transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			Camera.current.transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			Camera.current.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+		}
+
+// TODO: figure out how to look left and right by rotating on Y axis (use mouse?)
+/*		var r : float = speed * Input.GetAxis ("Mouse Y");
+		transform.Rotate (r, 0, 0);
+*/
+	}
+}
