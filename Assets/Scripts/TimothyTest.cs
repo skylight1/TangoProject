cf54fb67a3611c359927786e5987d3d14df56209
dariol <dario.nycjava@gmail.com>
2015-09-10 14:38:36 -0400

    added Elliot's code (not attached at runtime)

--- Assets/Scripts/TimothyTest.cs
+++ Assets/Scripts/TimothyTest.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 public class TimothyTest : MonoBehaviour {
 
+	/////
+	bool DEBUG = true;
+	/////
+
 	public class DistanceAndPoint {
 		public Vector3 point;
 		public double distance;
@@ -22,6 +26,10 @@ public class TimothyTest : MonoBehaviour {
 	private bool m_onTarget;
 	private Vector3 targetPosition;
 	private Quaternion targetRotation;
+
+	/////
+	int nupdates = 0;
+	/////
 
 	// Use this for initialization
 	void Start () {
@@ -48,6 +56,9 @@ public class TimothyTest : MonoBehaviour {
 	private void updateTargetPositionAndRotation() {
 		
 		Vector3 p1 = m_camera.transform.position;
+		/////
+		Vector3 p1forward = m_camera.transform.forward;
+		/////
 		Vector3 p2 = m_camera.transform.position + m_camera.transform.forward;
 		
 		List<DistanceAndPoint> distancesAndPoints = new List<DistanceAndPoint>();
@@ -84,7 +95,49 @@ public class TimothyTest : MonoBehaviour {
 		Vector3 c3 = distancesAndPoints[2].point;
 		
 		Vector3 cNormal = Vector3.Cross (c1 - c3, c2 - c3);
+
+		///////////
+		if (DEBUG) {
+			if (++nupdates == 100 | nupdates % 500 == 0) {
+				for (int i=0; i < distancesAndPoints.Count; i++) {
+					Debug.Log ("TangoUpdate ***** point number ," + i + 
+					           "," + distancesAndPoints [i].point.x +
+					           "," + distancesAndPoints [i].point.y +
+					           "," + distancesAndPoints [i].point.z +
+//					           "," + distancesAndPoints [i].angle +
+					           "," + distancesAndPoints [i].distance);
+				}
+			}
+			;
+			
+			/*
+			Debug.Log ("TangoUpdate number of points " + distancesAndPoints.Count);
+			Debug.Log ("TangoUpdate min distance " + distancesAndPoints [0].distance);
+			Debug.Log ("TangoUpdate max distance " + distancesAndPoints [distancesAndPoints.Count - 1].distance);
+			
+			Debug.Log ("TangoUpdate center " + centerPoint.x + ", " + centerPoint.y + ", " + centerPoint.z);
+			Debug.Log ("TangoUpdate target " + targetPoint.x + ", " + targetPoint.y + ", " + targetPoint.z);
+			Debug.Log ("TangoUpdate point 1 " + c1.x + ", " + c1.y + ", " + c1.z);
+			Debug.Log ("TangoUpdate point 2 " + c2.x + ", " + c2.y + ", " + c2.z);
+			Debug.Log ("TangoUpdate point 3 " + c3.x + ", " + c3.y + ", " + c3.z);
+			Debug.Log ("TangoUpdate camera " + p1.x + ", " + p1.y + ", " + p1.z);
+			Debug.Log ("TangoUpdate view direction " + p1forward.x + ", " + p1forward.y + ", " + p1forward.z);
+			Debug.Log ("TangoUpdate cNormal " + cNormal.x + ", " + cNormal.y + ", " + cNormal.z);
+			*/
+		}
+		double[] pcaPlane = fitNearestSurface (distancesAndPoints, 30);
+		Vector3 pcaPlanev3 = new Vector3 ((float) pcaPlane [0], (float) pcaPlane [1], (float) pcaPlane [2]);
 		
+		if (DEBUG) Debug.Log ("TangoUpdate pcaPlane " + pcaPlanev3.x + ", " + pcaPlanev3.y + ", " + pcaPlanev3.z);
+
+		// replace -cNormal plane with the pcaPlane
+		
+		//		Vector3 cNormal = Vector3.Cross (c1 - c3, c2 - c3);
+//		m_bounceSurface.transform.rotation = Quaternion.FromToRotation (Vector3.up, -pcaPlanev3);
+
+		//////////
+
+			
 		targetRotation = Quaternion.FromToRotation (Vector3.up, -cNormal);
 		
 		targetPosition = (c1 + c2 + c3) / 3;
@@ -98,4 +151,137 @@ public class TimothyTest : MonoBehaviour {
 		
 		m_bounceSurface.transform.position = targetPosition;
 	}
+
+
+	/////////////////////////////////////
+	private Vector3 V3mult(Vector3 v1, Vector3 v2) {
+		Vector3 vmult = new Vector3 (v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+		return vmult;
+	}
+	
+	private Vector3 V3sqrt(Vector3 v1) {
+		Vector3 vsqrt = new Vector3 (Mathf.Sqrt (v1.x), Mathf.Sqrt (v1.y), Mathf.Sqrt (v1.z));
+		return vsqrt;
+	}
+	
+	private double[] fitNearestSurface(List<DistanceAndPoint> distancesAndPoints, int n) {
+		if (DEBUG) Debug.Log ("TangoUpdate pca , for " + n + " points");
+		MatrixStats mc = getCovar (distancesAndPoints, n);
+		
+		OnePC pc1 = powerMethod(mc.covar);
+		
+		if (DEBUG) Debug.Log ("TangoUpdate pca1 " + pc1.pc[0] + ", " + pc1.pc[1] + ", " + pc1.pc[2]);
+		
+		double[,] covar2 = new double[3,3];
+		for (int i=0; i < 3; i++) {
+			for (int j=0; j < 3; j++) {
+				covar2[i,j] = mc.covar[i,j] - ( pc1.shrink * pc1.pc[i] * pc1.pc[j] );
+			}
+		}
+		
+		OnePC pc2 = powerMethod(covar2);
+		
+		if (DEBUG) Debug.Log ("TangoUpdate pca2 " + pc2.pc[0] + ", " + pc2.pc[1] + ", " + pc2.pc[2]);
+		
+		// 	c(x=y1 * z2 - z1 * y2, y=z1 * x2 - x1 * z2, z=x1 * y2 - y1 * x2)
+		double[] planeCoefficients = {
+			pc1.pc [1] * pc2.pc [2] - pc1.pc [2] * pc2.pc [1],
+			pc1.pc [2] * pc2.pc [0] - pc1.pc [0] * pc2.pc [2],
+			pc1.pc [0] * pc2.pc [1] - pc1.pc [1] * pc2.pc [0],
+			0.0f
+		};
+		
+		planeCoefficients[3] = planeCoefficients[0] * mc.mean[0] + planeCoefficients[1] * mc.mean[1] + planeCoefficients[2] * mc.mean[2];
+		
+		return planeCoefficients;
+	}
+	
+	private MatrixStats getCovar(List<DistanceAndPoint> distancesAndPoints, int n) {
+		if (DEBUG) Debug.Log ("TangoUpdate pca start getCovar");
+		
+		Vector3 sum = new Vector3(0.0f, 0.0f, 0.0f);
+		for (int i=0; i < n; i++) {
+			sum += distancesAndPoints[i].point;
+		}
+		
+		if (DEBUG) Debug.Log ("TangoUpdate pca vector sum " + sum.x + ", " + sum.y + ", " + sum.z);
+		
+		double[] mean = {sum.x / n, sum.y / n, sum.z / n};
+		
+		if (DEBUG) Debug.Log ("TangoUpdate pca vector mean " + mean[0] + ", " + mean[1] + ", " + mean[2]);
+		
+		double[,] covar = {{0,0,0}, {0,0,0}, {0,0,0}};
+		
+		if (DEBUG) Debug.Log ("TangoUpdate pca initial, " + covar[0,0] + "," + covar[0,1] + "," + covar[1,2]);
+		
+		for (int i=0; i < n; i++) {
+			covar[0,0] += (distancesAndPoints[i].point.x - mean[0]) * (distancesAndPoints[i].point.x - mean[0]);
+			covar[0,1] += (distancesAndPoints[i].point.x - mean[0]) * (distancesAndPoints[i].point.y - mean[1]);
+			covar[0,2] += (distancesAndPoints[i].point.x - mean[0]) * (distancesAndPoints[i].point.z - mean[2]);
+			covar[1,1] += (distancesAndPoints[i].point.y - mean[1]) * (distancesAndPoints[i].point.y - mean[1]);
+			covar[1,2] += (distancesAndPoints[i].point.y - mean[1]) * (distancesAndPoints[i].point.z - mean[2]);
+			covar[2,2] += (distancesAndPoints[i].point.z - mean[2]) * (distancesAndPoints[i].point.z - mean[2]);
+		}
+		covar [1, 0] = covar [0, 1];
+		covar [2, 0] = covar [0, 2];
+		covar [2, 1] = covar [1, 2];
+		
+		if (DEBUG) Debug.Log ("TangoUpdate pca , " + covar[0,0] + "," + covar[0,1] + "," + covar[1,2]);
+		
+		for (int i=0; i < 3; i++) {
+			for (int j=0; j < 3; j++) {
+				covar [i, j] /= (n - 1);
+				if (DEBUG) Debug.Log ("TangoUpdate pca , " + i + "," + j + "," + covar[i,j]);
+			}
+		}
+		
+		
+		MatrixStats mstats = new MatrixStats (mean, covar);
+		
+		return mstats;
+	}
+	
+	private OnePC powerMethod(double[,] covar) {
+		
+		double sqr3 = 1.0f / Mathf.Sqrt (3.0f);
+		double[] pc1 = {sqr3, sqr3, sqr3};
+		double shrink = 0.0;
+		for (int k = 0; k < 20; k++) {
+			double[] pc1a = {0.0f, 0.0f, 0.0f};
+			for (int i=0; i < 3; i++) {
+				for (int j=0; j < 3; j++) {
+					pc1a[i] += covar[i,j] * pc1[j];
+				}
+			}
+			double shrink2 = pc1a[0] * pc1a[0] + pc1a[1] * pc1a[1] + pc1a[2] * pc1a[2];
+			float shrink2f = (float) shrink2;
+			shrink = Mathf.Sqrt(shrink2f);
+			for (int i=0; i < 3; i++) {
+				pc1[i] = pc1a[i] / shrink;
+			}
+		}
+		OnePC pc = new OnePC (pc1, shrink);
+		return pc;
+	}
+	
+	public class MatrixStats {
+		public double[] mean;
+		public double[,] covar;
+		
+		public MatrixStats(double[] mean, double[,] covar) {
+			this.mean = mean;
+			this.covar = covar;
+		}
+	}
+	
+	public class OnePC {
+		public double[] pc;
+		public double shrink;
+		
+		public OnePC(double[] pc, double shrink) {
+			this.pc = pc;
+			this.shrink = shrink;
+		}
+	}
+	//////////////////////////////////////////
 }
