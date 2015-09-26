fbafa6ed58443ce10baf55f35cbbecc93353e6e8
megbeckim <timothy@Timothys-Air.fios-router.home>
2015-09-26 19:54:26 -0400

    with cup and collider

---
+++ Assets/BottomOfCup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomOfCup : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("SCORE!!!!!");
+		other.gameObject.SetActive (false);
+	}
+}
